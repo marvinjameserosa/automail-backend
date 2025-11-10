49294e21403b7c249abfabbc469e6649c9da9408
marvinjameserosa <marvinjameserosa@gmail.com>
2025-11-10 12:20:05 +0800

    refactor(email): improve structure of fastapi

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from .routers import example
 
 app = FastAPI()
 
@@ -16,7 +17,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(example.router)
